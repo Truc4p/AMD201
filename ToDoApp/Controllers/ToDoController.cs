11c6d8f67570383c2bd74089031a9e90ac3bac46
Truc4p <truc4pham@gmail.com>
2024-10-11 15:42:25 +0700

    Add .gitignore file to exclude Visual Studio specific files

---
+++ ToDoApp/Controllers/ToDoController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        // In-memory list to simulate a database
+        private static List<ToDoItem> toDoItems = new List<ToDoItem>
+        {
+            new ToDoItem { Id = 1, Title = "Learn ASP.NET Core", IsCompleted = false },
+            new ToDoItem { Id = 2, Title = "Build a To-Do App", IsCompleted = true }
+        };
+
+        // 1. Read - Display all ToDo Items
+        public IActionResult Index()
+        {
+            return View(toDoItems);
+        }
+
+        // 2. Create - Show form to create a new ToDo item
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // 2. Create - Handle form submission for new ToDo item
+        [HttpPost]
+        public IActionResult Create(ToDoItem item)
+        {
+            item.Id = toDoItems.Count + 1; // Generate a new Id for the item
+            toDoItems.Add(item);
+            return RedirectToAction("Index");
+        }
+
+        // 3. Update - Show form to edit an existing ToDo item
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var item = toDoItems.FirstOrDefault(i => i.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        // 3. Update - Handle form submission for editing an existing ToDo item
+        [HttpPost]
+        public IActionResult Edit(ToDoItem updatedItem)
+        {
+            var item = toDoItems.FirstOrDefault(i => i.Id == updatedItem.Id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            item.Title = updatedItem.Title;
+            item.IsCompleted = updatedItem.IsCompleted;
+            return RedirectToAction("Index");
+        }
+
+        // 4. Delete - Show confirmation page for deleting an item
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var item = toDoItems.FirstOrDefault(i => i.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        // 4. Delete - Handle deletion of an item
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var item = toDoItems.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                toDoItems.Remove(item);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }         // Unique identifier for each item
+        public string Title { get; set; }   // The title of the To-Do task
+        public bool IsCompleted { get; set; } // Is the task completed?
+    }
+}
